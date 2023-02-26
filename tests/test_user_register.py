a52ad90daa5f47751524aa010e73cdb469680ec0
DmitryAsl <aslamovdima10@yandex.ru>
2023-02-26 15:08:00 +0300

    add TestUserRegister of homework

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -3,8 +3,16 @@ import requests
 from lib.BaseCase import BaseCase
 from lib.assertions import Assertions
 from datetime import datetime
+import random
 
 class TestUserRegister(BaseCase):
+    fields = [
+        ({'firstName': 'learnqa', 'lastName': 'learnqa', 'email': "test@example.com", 'password': '1234'}),
+        ({'username': 'learnqa', 'lastName': 'learnqa', 'email': "test@example.com", 'password': '1234'}),
+        ({'username': 'learnqa', 'firstName': 'learnqa', 'email': "test@example.com", 'password': '1234'}),
+        ({'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'password': '1234'}),
+        ({'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': "test@example.com"})
+    ]
 
     def setup_method(self):
         base_part = "learnqa"
@@ -40,3 +48,54 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_status_code(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists"
+
+    def test_create_user_with_incorrect_email(self):
+        email = 'testexample.com'
+        data = {
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email,
+            'password': '1234'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format"
+
+    @pytest.mark.parametrize("data", fields)
+    def test_create_user_without_required_field(self, data):
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert "The following required params are missed:" in response.content.decode("utf-8"), f"Unexpected response content: {response.content}"
+
+    def test_create_user_with_short_username(self):
+        data = {
+            'username': 'I',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email,
+            'password': '1234'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'username' field is too short"
+
+    def test_create_user_with_longest_usertname(self):
+        username = '251_chapters_of_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_username_long_userna'
+        data = {
+            'username': username,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email,
+            'password': '1234'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'username' field is too long"
